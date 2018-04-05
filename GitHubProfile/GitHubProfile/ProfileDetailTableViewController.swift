375149bbf9bba3f0b2c855b929f3628042b362f5
Israel3D <Israel3D@macbook-pro-de-israel.local>
2018-04-05 00:29:42 -0300

    Developing TableView

--- GitHubProfile/GitHubProfile/ProfileDetailTableViewController.swift
+++ GitHubProfile/GitHubProfile/ProfileDetailTableViewController.swift
@@ -19,11 +19,7 @@ class ProfileDetailTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-    }
-    override func viewWillAppear(_ animated: Bool) {
+        
         //jeremy
         if let url = URL(string: "https://api.github.com/users/\(username!)/repos") {
             let task = URLSession.shared.dataTask(with: url, completionHandler: { (dataRtd, res, error) in
@@ -33,7 +29,7 @@ class ProfileDetailTableViewController: UITableViewController {
                         do {
                             
                             if let objJson =  try JSONSerialization.jsonObject(with: data, options:[]) as? NSArray {
-                               // print(objJson)
+                                // print(objJson)
                                 
                                 var i = 0
                                 var projects:[String:Any] = [:]
@@ -43,12 +39,14 @@ class ProfileDetailTableViewController: UITableViewController {
                                     self.projectDetails.append(project)
                                     i+=1
                                 }
-                                let name = projects["owner"] as? [String: Any]
-                                let user = UserProfile(image: #imageLiteral(resourceName: "moeda_coroa"), name: name!["login"] as? String ?? "Name")
+                                // let name = projects["owner"] as? [String: Any]
+                                //                                let user = UserProfile(image: #imageLiteral(resourceName: "moeda_coroa"), name: name!["login"] as? String ?? "Name")
+                                let user = UserProfile(image: #imageLiteral(resourceName: "moeda_coroa"), name: self.username)
                                 
                                 DispatchQueue.main.async {
                                     self.lblUser.text = user.name
                                     self.userImageView.image = user.image
+                                    self.tableView.reloadData()
                                 }
                             }
                             
@@ -65,7 +63,9 @@ class ProfileDetailTableViewController: UITableViewController {
             })
             task.resume()
         }
+        
     }
+    
 
     func showAlertMessage(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
@@ -83,68 +83,19 @@ class ProfileDetailTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return projectDetails.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseCell", for: indexPath)
+        let project = projectDetails[indexPath.row]
+        cell.textLabel?.text = project.titulo
+        cell.detailTextLabel?.text = project.descricao
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+ 
 }
